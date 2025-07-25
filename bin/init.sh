51131e9226dfa70ab3eeb27d54b03ccc5be4b391
Ian Rasbheri <ian@irasbheri.lan>
2025-07-25 09:21:59 +0100

    Add initial slots

---
+++ bin/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+uthisha add --time 9:20 --code OPEN --type open
+uthisha add --time 9:30 --code WELCOME
+
+uthisha add --time 10:40 --duration 20 --code AMBREAK
+
+uthisha add --time 12:20 --duration 60 --code LUNCH
+
+uthisha add --time 14:30 --duration 20 --code PMBREAK
+
+uthisha add --time 15:50 --code WRAP
+uthisha add --time 16:00 --code CLOSE --type close
+
